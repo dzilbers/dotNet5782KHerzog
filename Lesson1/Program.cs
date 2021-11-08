c6eafce9b15f929bc2ccb2a3cc98bd110df5d82c
dzilb <dzilb@DZILBERS1>
2021-11-08 12:47:54 +0200

    Add project files.

---
+++ Lesson1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+Console.WriteLine("Hello World!");
+
+Console.Read();
+
+Object obj = new Object();
+Dabush a = new ();
+Console.WriteLine(obj);
+Console.WriteLine(a);
+
+MyStruct str;// = new(); // { Num = 3 };
+str.Num = 20;
+str.D = new();
+Console.WriteLine(str);
+
+object obj2 = 11;
+int num2 = (int)obj2;
+
+struct MyStruct
+{
+    public int Num;
+    public Dabush D;
+}
+
+class Dabush {
+    int i1 = 10;
+    static int si1 = 99;
+
+    public Dabush(int n) => i1 = n;
+    public Dabush() { }
+
+    public override string ToString()
+    {
+        return "Itay Dabush from K.Herzog";
+    }
+}
+
+class Son : Dabush
+{
+    public Son() // : base(-8)
+    {
+        Console.WriteLine("Son ctor");
+    }
+}
+
+
+
---
+++ Lesson20211107/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Lesson20211107
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i1 = 1, i2, i3 = 3;
+            Console.WriteLine(func(ref i1, out i2, in i3));
+            Console.WriteLine(sum());
+            Console.WriteLine(sum(1));
+            Console.WriteLine(sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+
+            named(1, parm3: "Adir", parm2: 2.5);
+            named(1, parm3: "Yair");
+
+            outer();
+
+
+            int[] arr = { 1, 2, 3, 4, 5 };
+            //var (a, b, rest) = arr;
+
+            MyClass1 o1 = new();
+            MyClass2 o2 = (MyClass2)o1;
+        }
+
+        static (int, double) intDouble(in string s)
+        {
+            return (int.Parse(s), double.Parse(s));
+        }
+
+        static int switchy(object o) => o switch
+        {
+            int => (int)o,
+            string => int.Parse("" + o),
+            _ => 0
+        };
+
+        static int field = 9;
+        static void outer()
+        {
+            int number = 99;
+            Console.WriteLine("Outer: " + field + number);
+            inner();
+            sInner();
+            void inner() { Console.WriteLine("Inner" + field + number); }
+            static void sInner() { Console.WriteLine("Static Inner" + field /*+ number*/); }
+        }
+
+        static void named(int parm1, double parm2 = 0.0, string parm3 = "Daniel")
+        {
+            Console.WriteLine("" + parm1 + parm2 + parm3);
+        }
+
+        static int func(ref int ref1, out int out2, in int in3)
+        {
+            out2 = in3; // mandatory to assign a value
+            ref1 = out2; // possible but not mandatory
+            //in3 = 3; // error
+            return -1;
+        }
+
+        static int sum(params int[] numbers)
+        {
+            int s = 0;
+            foreach (int num in numbers)
+                s += num;
+            return s;
+        }
+
+    }
+
+    class MyClass1 { }
+    class MyClass2 {
+        public static explicit operator MyClass2(MyClass1 o) => new MyClass2();
+    }
+
+}
